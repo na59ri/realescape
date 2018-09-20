65cdeb5c4f80775da32f5999f2f15ae8ff37bfc0
na59ri <32233567+na59ri@users.noreply.github.com>
2018-09-21 08:31:23 +0900

    add log

--- realescape.py
+++ realescape.py
@@ -3,10 +3,12 @@ app = Flask(__name__)
 
 @app.route("/clova/webhook")
 def hello():
+    print("/clova/webhook start")
     return "Hello World!!"
 
 @app.route("/bot/webhook")
 def hello():
+    print("/bot/webhook start")
     return "Hello World!!"
 
 if __name__ == "__main__":
