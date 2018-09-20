87ba98e19659a1934499016404d80eb341b72860
na59ri <32233567+na59ri@users.noreply.github.com>
2018-09-21 08:38:23 +0900

    add comment

--- realescape.py
+++ realescape.py
@@ -1,15 +1,18 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/clova/webhook")
 def hello():
     print("/clova/webhook start")
-    return 'OK'
+    return '200'
+
 
 @app.route("/bot/webhook")
 def hello():
     print("/bot/webhook start")
-    return 'OK'
+    return '200'
+
 
 if __name__ == "__main__":
     app.run()
